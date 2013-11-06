4b4ceacd7db9662d5c36306e93e580bd146ee065
Alex Doub <alex@rocketmade.com>
2013-11-05 19:00:44 -0700

    Initial commit

---
+++ CryptoWallet/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  CryptoWallet
+//
+//  Created by Alex Doub on 11/5/13.
+//  Copyright (c) 2013 Alex Doub. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ CryptoWallet/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  CryptoWallet
+//
+//  Created by Alex Doub on 11/5/13.
+//  Copyright (c) 2013 Alex Doub. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
